1c4ddb51aa5fccacdc3f2221bdeb9ca8d375dff4
Yaseen Zahid <yaseen197@hotmail.com>
2024-04-03 15:38:27 -0400

    briefcase and clock now open when solved

--- src/worlds/Escape_Lab/scripts/lock.js
+++ src/worlds/Escape_Lab/scripts/lock.js
@@ -1,3 +1,6 @@
-function changeModel() {
-    document.getElementById('Artifact-Briefcase').setAttribute('gltf-model', 'obj: #briefcaseOpen');
+function openCase() {
+    document.getElementById('Artifact-Briefcase').setAttribute('gltf-model', 'assets/models/briefcaseOpen_obj.glb');
+}
+function openClock() {
+    document.getElementById('Artifact-Clock').setAttribute('gltf-model', 'assets/models/clockOpen_obj.glb');
 }
